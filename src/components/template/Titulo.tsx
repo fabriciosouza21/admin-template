2059b5da991b21c610a99771f19c84db4d9dd566
unknown <fabriciosouza21777@gmail.com>
2021-08-30 18:22:58 -0300

    Estrutura inicial

---
+++ src/components/template/Titulo.tsx
@@ -0,0 +1,19 @@
+
+interface tituloProps{
+    titulo: string
+    subtitulo: string
+}
+
+export default function Titulo(props: tituloProps){
+    return (
+        <div>
+            <h1 className={` `}
+            >{props.titulo}
+            </h1>
+
+            <h2 className={``}>
+                {props.subtitulo}
+            </h2>
+        </div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
+import Layout from "../components/template/layout";
+
 export default function Home() {
   return (
-    <div className={`bg-gradient-to-r from-green-500 to-blue-500`} >
-      <h1>admin</h1>
-        
-    </div>
+    <Layout titulo = "Pagina inicial" subtitulo = "construindo um tamplate admin">
+        <h3>conteudo</h3>
+    </Layout>
   )
 }
