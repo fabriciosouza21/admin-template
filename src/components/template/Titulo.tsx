6e1a6b28ddbcdba1fd665f542db7ef53260f3206
fabricio21777 <fabriciosouza21777@gmail.com>
2021-09-01 18:29:31 -0300

    melhorando o componente titulo

--- src/components/template/Titulo.tsx
+++ src/components/template/Titulo.tsx
@@ -7,11 +7,13 @@ interface tituloProps{
 export default function Titulo(props: tituloProps){
     return (
         <div>
-            <h1 className={` `}
+            <h1 className={`
+            font-black text-3xl 
+            text-gray-900 `}
             >{props.titulo}
             </h1>
 
-            <h2 className={``}>
+            <h2 className={`font-light text-sm text-gray-600`}>
                 {props.subtitulo}
             </h2>
         </div>
