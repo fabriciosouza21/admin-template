aab50a6c60394feb5c07193bf1051df88780f424
fabricio21777 <fabriciosouza21777@gmail.com>
2021-09-08 18:35:41 -0300

    criando componente botão alterar tema

--- src/components/template/Cabecalho.tsx
+++ src/components/template/Cabecalho.tsx
@@ -1,3 +1,5 @@
+import useAppData from "../../data/hook/useAppData";
+import BotaoTema from "./BotaoTema";
 import Titulo from "./Titulo";
 
 interface CabecalhoProps{
@@ -6,9 +8,14 @@ interface CabecalhoProps{
 }
 
 export default function Cabecalho(props: CabecalhoProps){
+    const {alternaTema,tema}=useAppData() 
     return (
-        <div>
+        <div className={`flex `}>
             <Titulo titulo = {props.titulo} subtitulo = {props.subtitulo}/>
+            <div className={`flex flex-grow justify-end`}>
+
+                <BotaoTema tema= {tema} altenarTema = {alternaTema} />
+            </div>
         </div>
     )
 }
